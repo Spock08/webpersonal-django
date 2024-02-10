995ab2d2509fbba4d9fe901ab88695ea9bbbb805
Emilio Ojeda <emiliojedal@gmail.com>
2024-02-10 15:31:13 -0300

    feature: add template html files and include core as installed apps in settings

--- core/views.py
+++ core/views.py
@@ -2,45 +2,14 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 
-html_base = """
-<h1>Mi web personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>    
-    <li><a href="/about/">About me</a></li>
-    <li><a href="/portfolio/">Portfolio</a></li>
-    <li><a href="/contact/">Contact</a></li>    
-</ul>
-"""
-
 def home(request):
-    return HttpResponse(
-        html_base + """
-        <h2>Portada</h2>
-        <p>Esto es la portada</p>
-        """
-    )
-
+    return render(request, "core/home.html")
 
 def about(request):
-    return HttpResponse(
-        html_base + """
-        <h2>About Me</h2>
-        <p>Mi Nombre es Emilio</p>
-        """
-    )
+    return render(request, "core/about.html")
 
 def portfolio(request):
-    return HttpResponse(
-        html_base + """
-        <h2>Portfolio</h2>
-        <p>Estos son mis trabajos</p>
-        """
-    )
+    return render(request, "core/portfolio.html")
 
 def contact(request):
-    return HttpResponse(
-        html_base + """
-        <h2>Contact</h2>
-        <p>Escriba al mail: <a href="mailto:hola@hola.com">hola@hola.com</a></p>
-        """
-    )
+    return render(request, "core/contact.html")
