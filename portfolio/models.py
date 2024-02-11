5265f1cbf2dc5b34a8c34552d091ce789edb934a
Emilio Ojeda <emiliojedal@gmail.com>
2024-02-11 13:54:23 -0300

    feat: include verbose_name in models to improve spanish version of the admin panel

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,11 +4,11 @@ from django.db import models
 
 
 class Project(models.Model):
-    title = models.CharField(max_length=200)
-    description = models.TextField()
-    image = models.ImageField()
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de Creación")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de Edición")
 
     class Meta:
         verbose_name = 'Proyecto'
